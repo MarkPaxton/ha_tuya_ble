5928f3f635cfe24162f41e14813406262ccef1a0
Mark Paxton <MarkPaxton@users.noreply.github.com>
2024-05-12 13:42:47 +0200

    Update const.py

--- custom_components/tuya_ble/const.py
+++ custom_components/tuya_ble/const.py
@@ -11,6 +11,8 @@ DEVICE_METADATA_UUIDS: Final = "uuids"
 DEVICE_DEF_MANUFACTURER: Final = "Tuya"
 SET_DISCONNECTED_DELAY = 10 * 60
 
+CONF_ACCESS_ID: Final = "access_id"
+CONF_ACCESS_SECRET: Final = "access_secret"
 CONF_UUID: Final = "uuid"
 CONF_LOCAL_KEY: Final = "local_key"
 CONF_CATEGORY: Final = "category"
